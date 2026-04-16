286d589d4b0449ef58cc07e3f7cc98479c113b08
gaoliang guo <guogaoliang@gmail.com>
2026-04-16 23:47:18 +0800

    fix: remove all Neo4j files and seed scripts that reference neo4j-driver

--- packages/db/src/neo4j/queries/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./circles.js";
-export * from "./projects.js";
-export * from "./relationships.js";
-export * from "./paths.js";
-export * from "./requests.js";
-export * from "./merit.js";
-export * from "./recommendations.js";
